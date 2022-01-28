11572014d4e85450ccddd936b28cad8632609924
Birgitta <birgitta.kaplas@kolumbus.fi>
2022-01-28 21:47:49 +0200

    2.19 added tiny bit of css

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -45,12 +45,23 @@ const Filter = (props) => {
   )
 }
 
+const Notification = ({ message }) => {
+  if (message === null) return null
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMsg, setErrorMsg] = useState(null)
 
   useEffect(() => {
     personService
@@ -80,6 +91,10 @@ const App = () => {
           setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
+          setErrorMsg(`${personObject.name} was added`)
+          setTimeout(() => {
+            setErrorMsg(null)
+          }, 3000)
         })
     }
   }
@@ -94,6 +109,10 @@ const App = () => {
         setPersons(persons.map(p => p.id !== person.id ? p : updated))
         setNewName('')
         setNewNumber('')
+        setErrorMsg(`${person.name} was updated`)
+        setTimeout(() => {
+          setErrorMsg(null)
+        }, 3000)
       })
   }
 
@@ -103,6 +122,10 @@ const App = () => {
       console.log('sure')
       personService.delet(id)
       setPersons(persons.filter(person => person.id !== id))
+      setErrorMsg(`${persons.find(p => p.id === id).name} was deleted`)
+      setTimeout(() => {
+        setErrorMsg(null)
+      }, 3000)
     }
   }
 
@@ -128,6 +151,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMsg} />
       <Filter newFilter={newFilter} handleFilter={handleFilter} />
       <h2>add a new</h2>
       <PersonForm
