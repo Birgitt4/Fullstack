e1a03a36faa926fa4a65da7d50c5434741c3658d
Birgitta <birgitta.kaplas@kolumbus.fi>
2022-02-17 17:13:09 +0200

    5.2%

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -3,6 +3,9 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+/*
+
+*/
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -10,11 +13,19 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
-
+  
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.loggedUser
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -24,6 +35,9 @@ const App = () => {
 
     try {
       const user = await loginService.login({ username, password })
+      
+      window.localStorage.setItem('loggedUser', JSON.stringify(user))
+      
       setUser(user)
       setUsername('')
       setPassword('')
@@ -31,7 +45,10 @@ const App = () => {
       console.log('wrong username or password')
     }
   }
-
+  const logout = () => {
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+  }
 
   if (user === null) {
     return (
@@ -61,7 +78,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button onClick={logout}>logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
